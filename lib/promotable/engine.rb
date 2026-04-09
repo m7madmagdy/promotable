050ae95e356809326eb31ded980c8228c175e625
Mohamed Magdy <mohamedmagdysaber@gmail.com>
2026-04-09 18:17:17 +0200

    Remove unused migration initializer from Promotable engine

--- lib/promotable/engine.rb
+++ lib/promotable/engine.rb
@@ -12,11 +12,5 @@ module Promotable
         include Promotable::ActsAsPromoter
       end
     end
-
-    initializer "promotable.append_migrations" do |app|
-      config.paths["db/migrate"].expanded.each do |expanded_path|
-        app.config.paths["db/migrate"] << expanded_path unless app.config.paths["db/migrate"].include?(expanded_path)
-      end
-    end
   end
 end

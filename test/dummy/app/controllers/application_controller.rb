907f3e250506c317dbd4dca8b57746495069777c
Mohamed Magdy <mohamedmagdysaber@gmail.com>
2026-05-18 23:28:03 +0300

    wipe

--- test/dummy/app/controllers/application_controller.rb
+++ test/dummy/app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :exception # Protect against CSRF attacks by raising an exception.
 
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
